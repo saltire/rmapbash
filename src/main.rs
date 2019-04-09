0d403509b8aa80a9e56b4ab711d11e141772fc01
saltire sable <saltiresable@gmail.com>
2019-04-09 11:51:57 -0400

    Draw map of chunks in entire world directory
    Creepily, this code worked perfectly on the first test.

--- src/main.rs
+++ src/main.rs
@@ -39,33 +39,67 @@ mod image;
 //     }
 // }
 
-fn draw_world_region_map(worldpath: &Path) -> Result<&str, Box<Error>> {
-    println!("Drawing map from world dir {}", worldpath.display());
+// fn draw_world_region_map(worldpath: &Path) -> Result<&str, Box<Error>> {
+//     println!("Drawing map of regions from world dir {}", worldpath.display());
+
+//     let regions = data::read_regions(worldpath)?;
+
+//     let min_x = regions.iter().map(|(x, _)| x).min().unwrap();
+//     let max_x = regions.iter().map(|(x, _)| x).max().unwrap();
+//     let min_z = regions.iter().map(|(_, z)| z).min().unwrap();
+//     let max_z = regions.iter().map(|(_, z)| z).max().unwrap();
+//     let width = max_x - min_x + 1;
+//     let height = max_z - min_z + 1;
+
+//     let mut pixels: Vec<bool> = vec![false; (width * height) as usize];
+//     for (rx, rz) in regions.iter() {
+//         pixels[((rz - min_z) * width + (rx - min_x)) as usize] = true;
+//     }
+
+//     let outpath = Path::new("./world.png");
+//     let file = File::create(outpath)?;
+
+//     image::draw_tiny_map(pixels.as_slice(), width as u32, height as u32, file)?;
+
+//     Ok(outpath.to_str().unwrap())
+// }
+
+fn draw_world_chunk_map(worldpath: &Path) -> Result<&str, Box<Error>> {
+    println!("Drawing map of chunks from world dir {}", worldpath.display());
 
     let regions = data::read_regions(worldpath)?;
 
-    let min_x = regions.iter().map(|(x, _)| x).min().unwrap();
-    let max_x = regions.iter().map(|(x, _)| x).max().unwrap();
-    let min_z = regions.iter().map(|(_, z)| z).min().unwrap();
-    let max_z = regions.iter().map(|(_, z)| z).max().unwrap();
-    let width = max_x - min_x + 1;
-    let height = max_z - min_z + 1;
+    let min_rx = regions.iter().map(|(x, _)| x).min().unwrap();
+    let max_rx = regions.iter().map(|(x, _)| x).max().unwrap();
+    let min_rz = regions.iter().map(|(_, z)| z).min().unwrap();
+    let max_rz = regions.iter().map(|(_, z)| z).max().unwrap();
+    let cwidth = (max_rx - min_rx + 1) * 32;
+    let cheight = (max_rz - min_rz + 1) * 32;
 
-    let mut pixels: Vec<bool> = vec![false; (width * height) as usize];
-    for (x, z) in regions.iter() {
-        pixels[((z - min_z) * width + (x - min_x)) as usize] = true;
+    let mut pixels: Vec<bool> = vec![false; (cwidth * cheight) as usize];
+    for (rx, rz) in regions.iter() {
+        let regionpath = worldpath.join("region").join(format!("r.{}.{}.mca", rx, rz));
+        let regionpixels = data::read_region_chunks(&regionpath)?;
+
+        let ro = (rz - min_rz) * cwidth * 32 + (rx - min_rx) * 32;
+
+        for cx in 0..32 {
+            for cz in 0..32 {
+                pixels[(ro + cz * cwidth + cx) as usize] = regionpixels[(cz * 32 + cx) as usize];
+            }
+        }
     }
 
     let outpath = Path::new("./world.png");
     let file = File::create(outpath)?;
 
-    image::draw_tiny_map(pixels.as_slice(), width as u32, height as u32, file)?;
+    image::draw_tiny_map(pixels.as_slice(), cwidth as u32, cheight as u32, file)?;
 
     Ok(outpath.to_str().unwrap())
 }
 
 fn draw_region_chunk_map(regionpath: &Path) -> Result<&str, Box<Error>> {
-    println!("Drawing map from region file {}", regionpath.display());
+    println!("Drawing map of chunks from region file {}", regionpath.display());
 
     let pixels = data::read_region_chunks(regionpath)?;
 
@@ -85,7 +119,7 @@ fn main() {
 
         let result = match path.extension() {
             Some(ext) if ext == "mca" => draw_region_chunk_map(path),
-            _ => draw_world_region_map(path),
+            _ => draw_world_chunk_map(path),
         };
 
         match result {
