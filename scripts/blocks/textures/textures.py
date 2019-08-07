1bfd316a4a1fb6a6656005a687431581ab1b25bb
saltire sable <saltiresable@gmail.com>
2019-08-06 21:56:48 -0400

    Update a bunch more block shapes and colours
    Also simplify texture script and remove unused csv data

--- scripts/blocks/textures/textures.py
+++ scripts/blocks/textures/textures.py
@@ -16,44 +16,23 @@ def get_texturecolors():
         if imgfile[-4:] != '.png':
             continue
 
-        im = Image.open(os.path.join(blocktexdir, imgfile))
+        im = Image.open(os.path.join(blocktexdir, imgfile)).convert('RGBA')
         pix = im.load()
-        rr = []
-        gg = []
-        bb = []
-        aa = []
+        pixels = []
         for x in range(im.width):
             for y in range(im.height):
-                if im.mode == 'RGBA':
-                    r, g, b, a = pix[x, y]
-                    if a > 0:
-                        rr.append(r)
-                        gg.append(g)
-                        bb.append(b)
-                        aa.append(a)
-                elif im.mode == 'RGB':
-                    r, g, b = pix[x, y]
-                    rr.append(r)
-                    gg.append(g)
-                    bb.append(b)
-                elif im.mode == 'LA':
-                    r, a = pix[x, y]
-                    rr.append(r)
-                    aa.append(a)
+                if pix[x, y][3] > 0:
+                    pixels.append(pix[x, y])
 
-        if len(rr) == 0:
+        if len(pixels) == 0:
             color = (0, 0, 0, 0)
         else:
-            if im.mode in ['RGB', 'RGBA']:
-                color = (
-                    int(sum(rr) / len(rr)),
-                    int(sum(gg) / len(gg)),
-                    int(sum(bb) / len(bb)),
-                    int(sum(aa) / len(aa)) if im.mode == 'RGBA' else 255,
-                )
-            elif im.mode == 'LA':
-                l = int(sum(rr) / len(rr))
-                color = (l, l, l, int(sum(aa) / len(aa)))
+            color = (
+                int(sum(p[0] for p in pixels) / len(pixels)),
+                int(sum(p[1] for p in pixels) / len(pixels)),
+                int(sum(p[2] for p in pixels) / len(pixels)),
+                int(sum(p[3] for p in pixels) / len(pixels)),
+            )
 
         texturecolors[imgfile[:-4]] = color
 
@@ -66,5 +45,5 @@ if __name__ == '__main__':
     with open(os.path.join(currentdir, 'texturecolors.csv'), 'w') as csvfile:
         writer = csv.writer(csvfile)
 
-    for texture, color in texturecolors.items():
-        writer.writerow([texture, *color])
+        for texture, color in texturecolors.items():
+            writer.writerow([texture, *color])
