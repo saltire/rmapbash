131eaa6abd4a750886747ed8da942378d860642e
saltire sable <saltiresable@gmail.com>
2019-08-07 22:24:18 -0400

    Fix some colour bugs in isometric mode
    Use non-solid blocks' own light values instead of neighbours' for side colours.
    Lighting seems to be smoother overall, and a lot of black pixel bugs are fixed, particularly on grass.

--- src/isomap.rs
+++ src/isomap.rs
@@ -13,6 +13,8 @@ use super::world::World;
 
 fn draw_chunk(pixels: &mut [u8], blocktypes: &[BlockType], chunk: &region::Chunk, co: &isize,
     width: &usize, cblimits: &Edges<usize>, ylimits: &Range<usize>) {
+    let blank_color = RGBA::default();
+
     for bz in (cblimits.n..(cblimits.s + 1)).rev() {
         for bx in (cblimits.w..(cblimits.e + 1)).rev() {
             let bo2 = bz * BLOCKS_IN_CHUNK + bx;
@@ -31,32 +33,47 @@ fn draw_chunk(pixels: &mut [u8], blocktypes: &[BlockType], chunk: &region::Chunk
                     continue;
                 }
 
-                // Get the base color of the block.
+                // Create an index of colors corresponding to the digits in the block shape.
+                let mut bcolors = [&blank_color; 7];
+
+                // Get the block above this one.
                 let tblock = chunk.get_t_block(&by, &bo3, ylimits.end - 1);
-                let tcolor = &blocktype.colors[biome][tblock.slight][tblock.blight][1];
-                let tcolor2 = &blocktype.colors[biome][tblock.slight][tblock.blight][4];
                 // Don't draw the top if the block above is the same as this one and solid.
                 // This prevents stripes appearing in columns of translucent blocks.
                 let skip_top = tblock.btype == btype && blocktype.solid;
 
-                // Add a hilight if block to the left has skylight and is not solid.
-                let lblock = chunk.get_s_block(&bz, &bo3);
-                let lblocktype = &blocktypes[lblock.btype as usize];
-                let lshade = if lblock.slight > 0 && !lblocktype.solid { 2 } else { 1 };
-                let lcolor = &blocktype.colors[biome][lblock.slight][lblock.blight][lshade];
-                let lcolor2 = &blocktype.colors[biome][lblock.slight][lblock.blight][lshade + 3];
+                // Get the base color of the block, using light values from the block above.
+                // TODO: are there cases where it's preferable to use the block's own light values?
+                bcolors[1] = &blocktype.colors[biome][tblock.slight][tblock.blight][1];
+                bcolors[4] = &blocktype.colors[biome][tblock.slight][tblock.blight][4];
 
-                // Add a shadow if block to the right has skylight and is not solid.
-                // let rblock = chunk.get_s_block(&bz, &bo3);
-                let rblock = chunk.get_e_block(&bx, &bo3);
-                let rblocktype = &blocktypes[rblock.btype as usize];
-                let rshade = if rblock.slight > 0 && !rblocktype.solid { 3 } else { 1 };
-                let rcolor = &blocktype.colors[biome][rblock.slight][rblock.blight][rshade];
-                let rcolor2 = &blocktype.colors[biome][rblock.slight][rblock.blight][rshade + 3];
+                // If the block is solid, use light values from neighboring blocks for side colors.
+                // Otherwise use the block's own light values.
+                if blocktype.solid {
+                    // Add a hilight if block to the left has skylight and is not solid.
+                    let lblock = chunk.get_s_block(&bz, &bo3);
+                    let lblocktype = &blocktypes[lblock.btype as usize];
+                    let lshade = if lblock.slight > 0 && !lblocktype.solid { 2 } else { 1 };
+                    bcolors[2] = &blocktype.colors[biome][lblock.slight][lblock.blight][lshade];
+                    bcolors[5] = &blocktype.colors[biome][lblock.slight][lblock.blight][lshade + 3];
 
-                // Create an index of colors corresponding to the digits in the block shape.
-                let bcolors = [&RGBA::default(),
-                    &tcolor, &lcolor, &rcolor, &tcolor2, &lcolor2, &rcolor2];
+                    // Add a shadow if block to the right has skylight and is not solid.
+                    let rblock = chunk.get_e_block(&bx, &bo3);
+                    let rblocktype = &blocktypes[rblock.btype as usize];
+                    let rshade = if rblock.slight > 0 && !rblocktype.solid { 3 } else { 1 };
+                    bcolors[3] = &blocktype.colors[biome][rblock.slight][rblock.blight][rshade];
+                    bcolors[6] = &blocktype.colors[biome][rblock.slight][rblock.blight][rshade + 3];
+                } else {
+                    let light = chunk.data.lights[bo3];
+                    let slight = (light & 0x0f) as usize;
+                    let blight = ((light & 0xf0) >> 4) as usize;
+
+                    bcolors[2] = &blocktype.colors[biome][slight][blight][2];
+                    bcolors[5] = &blocktype.colors[biome][slight][blight][5];
+
+                    bcolors[3] = &blocktype.colors[biome][slight][blight][3];
+                    bcolors[6] = &blocktype.colors[biome][slight][blight][6];
+                }
 
                 let bpy = bpy2 + (MAX_BLOCK_IN_CHUNK_Y - by) * ISO_BLOCK_SIDE_HEIGHT;
 
